cc996d9a11d2cd5db1a83300629e97debc12ba64
LiedsonLB <108279994+LiedsonLB@users.noreply.github.com>
2022-06-27 13:21:25 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+const hamburgue = document.getElementById("checkbox");
+const menu = document.getElementsByClassName("menu")
+
+function translate(){
+
+}
+
+hamburgue.addEventListener("click", translate)
